ca149a288be6ac34784e73572ecd8d6cdb103fda
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2017-12-02 11:49:33 +0100

    [DEV] init Watson conv

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var watson = require('../config/watson')
+
+conversation = watson.config
+conversation.message({}, watson.processResponse);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
