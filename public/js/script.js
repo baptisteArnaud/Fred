fdb17bb3ac29b0aedd1e62df842f5448e1d9074a
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2018-02-21 11:17:32 +0100

    New design

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    var chatbox = document.getElementById("chatbox");
+
     $("#submit").click(function(e){
     	e.preventDefault();
     	message = $('#message').val();
@@ -8,12 +10,14 @@ $(document).ready(function(){
     	console.log(JSON.stringify(jsonMessage));
     	newUserMessage = "<div class='user'><div class='message-container'><article class='message'><div class='message-body'>"+message+"</div></article></div></div>";
     	$(".chatbox").append(newUserMessage);
+        chatbox.scrollTop = chatbox.scrollHeight;
         $.ajax({url: "/", 
         	type: "POST", 
         	data: jsonMessage,
         	success: function(data){ 
-        		newWatsonMessage = "<div class='watson'><div class='message-container'><article class='message'><div class='message-body watson'>"+data.message+"</div></article></div></div>"
+        		newWatsonMessage = "<div class='watson'><div class='message-container'><article class='message watson'><div class='message-body'>"+data.message+"</div></article></div></div>"
         		$(".chatbox").append(newWatsonMessage);
+                chatbox.scrollTop = chatbox.scrollHeight;
         	}, 
         	error: function(){ console.log('fail')}
         });
